e82bca559a3feac7be718520487a86961d4ff30e
nagi <nyagi.hime@gmail.com>
2019-03-25 13:00:16 +0900

    Fix typo

--- bin/colortest.sh
+++ bin/colortest.sh
@@ -3,7 +3,7 @@
 # tmux で使える色コード名の確認
 # @see https://superuser.com/questions/285381/how-does-the-tmux-color-palette-work
 for i in {0..255};
-do printf "\x1b[38;5;${i}mcolor%-5i\x1b[0m" $i ;
+do printf "\x1b[38;5;${i}mcolour%-5i\x1b[0m" $i ;
     if ! (( ($i + 1 ) % 8 )); then
         echo ;
     fi ;
