e981486714efbe3c7fa9ca3e96d601a7c0a81b7f
nagi <nyagi.hime@gmail.com>
2022-03-09 11:12:06 +0900

    remove zsh-autoswitch-virtualenv plugin

--- zsh/zsh.d/00400-zplug.zsh
+++ zsh/zsh.d/00400-zplug.zsh
@@ -16,8 +16,10 @@ autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
 # プラグインの有効化（個々に記述して設定ファイルをリロードすると自動的にインストールされる）
+# 削除したい場合は zinit delete <plugin name>
 zinit light zsh-users/zsh-completions                   # オートコンプリート
 zinit light zsh-users/zsh-autosuggestions               # コマンドサジェスト
 zinit light zdharma-continuum/fast-syntax-highlighting  # コマンドシンタックスハイライト
 zinit light g-plane/zsh-yarn-autocompletions            # yarn 補完
-zinit light MichaelAquilina/zsh-autoswitch-virtualenv   # python の仮想環境に入ると自動で activate
+# 自作のフックとバッティングするのでやめた
+# zinit light MichaelAquilina/zsh-autoswitch-virtualenv   # python の仮想環境に入ると自動で activate
